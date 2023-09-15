33d84984243d4d1c4b508eff59f9f3657e13a840
Pratham <chudasamapratham142@gmail.com>
2023-09-15 12:11:53 +0530

    update and getmembers profile

--- src/app/api/Route.ts
+++ src/app/api/Route.ts
@@ -1,3 +1,5 @@
-export function GET(){
-    
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({ status: true, message: "Hello world!!!" });
 }
