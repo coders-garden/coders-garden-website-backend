16d2608f6ffa9e1addfdd518423290d0b7dd5d13
Pratham <chudasamapratham142@gmail.com>
2023-09-24 13:54:04 +0530

    eslint formating

--- components/errorHandler.ts
+++ components/errorHandler.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import ResponseHandler from "./responseHandler";
 
-const errorHandler = (fn: (req: Request, res: Response, next?: NextFunction) => Promise<Response<any, Record<string, any>>>) =>
-    function (req: Request, res: Response, next: NextFunction) {
-        Promise.resolve(fn(req, res, next)).catch((err: Error) => {
-            return ResponseHandler.error({
-                req,
-                res,
-                message: err.message,
-            });
-        });
-    };
+const errorHandler = (
+	fn: (req: Request, res: Response, next?: NextFunction) => Promise<Response>
+) =>
+	function (req: Request, res: Response, next: NextFunction) {
+		Promise.resolve(fn(req, res, next)).catch((err: Error) => {
+			return ResponseHandler.error({
+				req,
+				res,
+				message: err.message,
+			});
+		});
+	};
 
 export default errorHandler;
