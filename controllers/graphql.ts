39d73d6be17362af2584dcaa8634ee04ea4cb93a
Pratham <chudasamapratham142@gmail.com>
2023-09-28 13:41:41 +0530

    fixed things and improved types

--- controllers/graphql.ts
+++ controllers/graphql.ts
@@ -1,30 +1,58 @@
-import { buildSchema } from "graphql";
+import {
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLList,
+	GraphQLBoolean,
+} from "graphql";
 import membersListLatest from "../data/members-list.json";
 import { Member } from "./member";
 
-const schema = buildSchema(`
-    type Member {
-        login: String!
-        name: String
-        tfa_enabled: Boolean!
-        is_public: Boolean!
-        role: String
-        last_active: String
-        saml_name_id: String
-        username: String
-        profile_pic_url: String
-        followers: String
-        following: String
-        repositories: String
-        bio: String
-        github_link: String
-    }
+/* eslint-disable camelcase */
 
-    type Query {
-        members: [Member!]!
-        member(username: String!): Member
-    }
-`);
+const MemberType = new GraphQLObjectType<Member>({
+	name: "Member",
+	fields: () => ({
+		login: { type: GraphQLString },
+		name: { type: GraphQLString },
+		tfa_enabled: { type: GraphQLBoolean },
+		is_public: { type: GraphQLBoolean },
+		role: { type: GraphQLString },
+		last_active: { type: GraphQLString },
+		saml_name_id: { type: GraphQLString },
+		username: { type: GraphQLString },
+		profile_pic_url: { type: GraphQLString },
+		followers: { type: GraphQLString },
+		following: { type: GraphQLString },
+		repositories: { type: GraphQLString },
+		bio: { type: GraphQLString },
+		github_link: { type: GraphQLString },
+	}),
+});
+
+const QueryType = new GraphQLObjectType({
+	name: "Query",
+	fields: {
+		members: {
+			type: new GraphQLList(MemberType),
+			resolve: () => membersListLatest,
+		},
+		member: {
+			type: MemberType,
+			args: {
+				username: { type: GraphQLString },
+			},
+			resolve: (root, { username }) =>
+				membersListLatest.find(
+					(member) => member.username === username
+				),
+		},
+	},
+});
+
+const schema = new GraphQLSchema({
+	query: QueryType,
+});
 
 const root = {
 	members: (): Member[] => {
